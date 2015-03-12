98fd43352475f00fdb723a463ef0ec18e9cfc895
Sean Niesen <seanniesen@gmail.com>
2015-03-12 13:39:54 -0700

    add times object to service

--- services.js
+++ services.js
@@ -2,6 +2,33 @@ var app = angular.module("sunrise-times.services", ['ui.bootstrap'])
 	.factory('dthreeService', function(){
     return {
     	makeGraph: function(array) {
+
+        var timesOfDay = [
+        {time: '12:00 am'},
+        {time: '1:00 am'},
+        {time: '2:00 am'},
+        {time: '3:00 am'},
+        {time: '4:00 am'},
+        {time: '5:00 am'},
+        {time: '6:00 am'},
+        {time: '7:00 am'},
+        {time: '8:00 am'},
+        {time: '9:00 am'},
+        {time: '10:00 am'},
+        {time: '11:00 am'},
+        {time: '12:00 pm'},
+        {time: '1:00 pm'},
+        {time: '2:00 pm'},
+        {time: '3:00 pm'},
+        {time: '4:00 pm'},
+        {time: '5:00 pm'},
+        {time: '6:00 pm'},
+        {time: '7:00 pm'},
+        {time: '8:00 pm'},
+        {time: '9:00 pm'},
+        {time: '10:00 pm'},
+        {time: '11:00 pm'}
+        ];
     		var w = 500;
 				var h = 300;
 				var barPadding = 1;

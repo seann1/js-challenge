0692a894bbece54269d8ef8807b73f0e99187446
Sean Niesen <seanniesen@gmail.com>
2015-03-16 21:41:41 -0700

    changing graph

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.controller('MainCtrl', [
 													}
 
 
-								    			timesObject.sunrise = toSeconds(toTimeZone(data.results.sunrise));
+								    			timesObject.sunrise = toTimeZone(data.results.sunrise);
    												timesObject.sunset = toSeconds(toTimeZone(data.results.sunset));
    												timesObject.dayLength = toSeconds(data.results.day_length);
    												timesObject.astroTwilightBegin = toSeconds(toTimeZone(data.results.astronomical_twilight_begin));
--- services.js
+++ services.js
@@ -3,19 +3,49 @@ var app = angular.module("sunrise-times.services", ['ui.bootstrap'])
     return {
     	makeGraph: function(array) {
 
-        var numbers = [];
-        var hours = [];
         var d3Dates = [];
 
         for (var i = 0; i < array.length; i++) {
           d3Dates.push(array[i].date);
         }
 
-        // for (var i = 0; i < timesOfDay.length; i++) {
-        //   numbers.push(i.number);
-        //   hours.push(i.number);
-        // } 
+        function dateChanger(date) {
+          var splitDate = date.split("-");
+          if (splitDate[0].length === 1) {
+            var index0 = ["0",splitDate[0]].join("");
+          } else {
+            var index0 = splitDate[0];
+          }
+          if (splitDate[1].length === 1) {
+            var index1 = ["0",splitDate[0]].join("");
+          } else {
+            var index1 = splitDate[1];
+          }
 
+          return [index0, index1, splitDate[2]].join("-");
+        };
+
+        function toTime(time) {
+          var splitTime = time.split(":");
+          if (splitTime[0].length === 1) {
+            var index0 = ["0",splitTime[0]].join("");
+          } else {
+            var index0 = splitTime[0];
+          }
+          if (splitTime[1].length === 1) {
+            var index1 = ["0",splitTime[0]].join("");
+          } else {
+            var index1 = splitTime[1];
+          }
+          var splitInd2 = splitTime[2].split(" ");
+          if (splitInd2[0].length === 1) {
+            var lastIndex = ['0',splitInd2[0]," ", splitInd2[1]].join("");
+          } else {
+            var lastIndex = splitInd2.join(" ");
+          }
+
+          return [index0, index1].join(":") + ":" + lastIndex;
+        };
 
   var vis = d3.select('#visualisation'),
     WIDTH = 700,
@@ -28,21 +58,22 @@ var app = angular.module("sunrise-times.services", ['ui.bootstrap'])
     },
     xRange = d3.time.scale().range([MARGINS.left, WIDTH - MARGINS.right]).domain([
         d3.min(array, function(d) {
-          return moment(d.date);
+          return d3.time.format("%m-%d-%Y").parse(dateChanger(d.date));
         }),
         d3.max(array, function(d) {
-          return moment(d.date);
+          return d3.time.format("%m-%d-%Y").parse(dateChanger(d.date));
         })
       ]),
-    // yRange = d3.time.scale().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([
-    //   d3.min(timesOfDay, function(d) {
-    //     return moment(d.hour);
-    //   }), 
-    //   d3.max(timesOfDay, function(d) {
-    //     return moment(d.hour);
-    //   })]),
 
-    yRange = d3.time.scale().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([moment({hour: 0}), moment({hour: 23})]),
+    yRange = d3.time.scale().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([
+      d3.min(array, function(d) {
+        return d3.time.format("%I:%M:%S %p").parse(toTime(d.sunrise))
+      }),
+      d3.max(array, function(d) {
+        console.log(d3.time.format("%I:%M:%S %p").parse(toTime(d.sunrise)));
+        return d3.time.format("%I:%M:%S %p").parse(toTime(d.sunrise));
+      })
+    ]),
 
     xAxis = d3.svg.axis()
       .scale(xRange)
@@ -59,6 +90,17 @@ var app = angular.module("sunrise-times.services", ['ui.bootstrap'])
       .tickFormat(d3.time.format("%I %p"))
       .orient('left')
       .tickSubdivide(true);
+
+    var valueline = d3.svg.line()
+      .interpolate("linear")
+      .x(function(d) {
+        return d3.time.format("%m-%d-%Y").parse(dateChanger(d.date));
+      })
+      .y(function(d) {
+        // var time = moment(d.sunrise).format("-MM-YYYY hh:mm:ss");
+        console.log(d3.time.format("%I:%M:%S %p").parse(toTime(d.sunrise)));
+        return d3.time.format("%I:%M:%S %p").parse(toTime(d.sunrise)); 
+      });
  
     vis.append('svg:g')
       .attr('class', 'x axis')
@@ -69,6 +111,12 @@ var app = angular.module("sunrise-times.services", ['ui.bootstrap'])
       .attr('class', 'y axis')
       .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
       .call(yAxis);
+      
+    vis.append('svg:path')
+      .attr('d', valueline(array))
+      .attr('stroke', 'blue')
+      .attr('stroke-width', 2)
+      .attr('fill', 'black');
     return vis;
     	}
     }
